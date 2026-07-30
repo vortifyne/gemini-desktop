b02529c5fac12c5a3f1770e3a11b01e394d1a10d
Nikita Sinepupov <vortifyne@gmail.com>
2026-07-30 15:26:26 +0700

    fix: remove all tests for Gemini API functionality

--- main.go
+++ main.go
@@ -2,14 +2,10 @@ package main
 
 import (
 	"embed"
-	"fmt"
 	"log"
-	"os"
 
-	"github.com/joho/godotenv"
 	"github.com/vortifyne/gemini-desktop/internal/bindings"
 	"github.com/vortifyne/gemini-desktop/internal/database"
-	"github.com/vortifyne/gemini-desktop/internal/gemini"
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
 	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
@@ -32,28 +28,6 @@ func main() {
 			log.Fatalf("Failed to close database: %v\n", err)
 		}
 	}(storage)
-
-	// Read API key
-	if err := godotenv.Load(); err != nil {
-		log.Println("No .env file found, using system ENVs")
-	}
-
-	apiKey := os.Getenv("GEMINI_API_KEY")
-
-	// Initialize Gemini Client
-	isValid, err := gemini.CheckGeminiKeyLive(apiKey)
-	if err != nil || !isValid {
-		log.Println("Key read from .env isn't valid")
-	}
-	client := gemini.NewGeminiClient(apiKey)
-	fmt.Println()
-
-	// Send first promt
-	respText, err := client.SendMessage("Write a the funniest joke about academic programming")
-	if err != nil {
-		log.Printf("Failed to send message from user: %v\n", err)
-	}
-	fmt.Println(respText)
 
 	// Create application with options
 	err = wails.Run(&options.App{
