d404c34a920089e6415caff7ee7c8f61b3c54275
Nikita <vortifyne@gmail.com>
2026-08-04 10:16:24 +0700

    chore: update product version (#23)

--- main.go
+++ main.go
@@ -55,8 +55,8 @@ func main() {
 	// Create application with options
 	err = wails.Run(&options.App{
 		Title:  "Gemini Desktop",
-		Width:  1024,
-		Height: 768,
+		Width:  1280,
+		Height: 720,
 		AssetServer: &assetserver.Options{
 			Assets: assets,
 		},
